4b4d95aecd9ad55e0ae05cda8b6ce782b20b868f
Priscila Carvalho <priferrari08@gmail.com>
2021-05-13 14:25:42 -0300

    primeiras mudanças

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled from 'styled-components';
+import {Filtro} from './components/Filtro';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+state = {
+valueMax : 2000,
+valueMin : 0,
+buscaProduto : "Camiseta",
+
+}
+  
+render() {
+  return(
+    <Filtro/>
+    );
+  }
 }
 
 export default App;
+  
+
+
+
