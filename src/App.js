3d4cdca04074e26dadc32e0a663bc8ecbe597823
Priscila Carvalho <priferrari08@gmail.com>
2021-05-13 14:51:21 -0300

    vamos tentando

--- src/App.js
+++ src/App.js
@@ -5,11 +5,26 @@ import {Filtro} from './components/Filtro';
 
 class App extends React.Component {
 state = {
-valueMax : 2000,
-valueMin : 0,
-buscaProduto : "Camiseta",
+valueMax : "",
+valueMin : "",
+buscaNome : "",
 
 }
+onChangevalueMin = (event) => {
+  this.setState({valueMin: event.target.value})
+}
+
+onChangevalueMax = (event) => {
+  this.setState({valueMax: event.target.value})
+}
+
+onChangebuscaNome = (event) => {
+  this.setState({buscaNome: event.target.value})
+}
+
+
+
+   
   
 render() {
   return(
--- src/components/Filtro.js
+++ src/components/Filtro.js
@@ -14,15 +14,15 @@ export class Filtro extends React.Component {
         <h2>Filtros</h2>
          <div>
              <h3>Valor Mínimo:</h3>
-             <input type= "number" value={this.props.valueMin} />
+             <input type= "number" value={this.props.valueMin} onChange={this.props.onChangevalueMin} />
          </div>
          <div>
              <h3>Valor Máximo:</h3>
-             <input type= "number" value={this.props.valueMax} />
+             <input type= "number" value={this.props.valueMax} onChange={this.props.onChangevalueMax}/>
          </div>
          <div>
              <h3>Busca por Nome:</h3>
-             <input type= "text" value={this.props.buscaProduto} />
+             <input type= "text" value={this.props.buscaProduto} onChange={this.props.onChangebuscaNome}/>
          </div>
          
      </ContainerDoFiltro>
